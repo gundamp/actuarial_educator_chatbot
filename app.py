2cf9d47aba1225d7f4497dc34fdc93eece036b6f
Jin <84253340+gundamp@users.noreply.github.com>
2023-12-17 10:13:11 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from dotenv import load_dotenv
+from dotenv import load_dotenv # Save your API key in the .env file in the same directory
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
@@ -32,14 +32,14 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="microsoft/phi-2")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    # llm = HuggingFaceHub(repo_id="microsoft/phi-2", model_kwargs={"temperature":0.0, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
@@ -66,7 +66,8 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
+
+    st.set_page_config(page_title="Chat with an Actuarial Educator",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
@@ -75,27 +76,27 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
+    st.header("Chat with an Actuarial Educator :books:")
+    user_question = st.text_input("Ask a question:")
     if user_question:
         handle_userinput(user_question)
 
     with st.sidebar:
-        st.subheader("Your documents")
+        st.subheader("Textbooks")
         pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+            "Upload textbooks here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                # get pdf text
+                
                 raw_text = get_pdf_text(pdf_docs)
 
-                # get the text chunks
+              
                 text_chunks = get_text_chunks(raw_text)
 
-                # create vector store
+                
                 vectorstore = get_vectorstore(text_chunks)
 
-                # create conversation chain
+               
                 st.session_state.conversation = get_conversation_chain(
                     vectorstore)
 
